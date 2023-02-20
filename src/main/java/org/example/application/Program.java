a11513a1a4de1eeb3f0c6c93d8701397809f8524
Neila Rodrigues <naal@gft.com>
2023-02-20 20:18:21 -0300

    Criação classe e testei

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,9 +1,12 @@
 package org.example.application;
 
+import org.example.bordgame.Position;
+
 public class Program {
 
     public static void main(String[] args){
 
-        System.out.println("Hello Word");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
