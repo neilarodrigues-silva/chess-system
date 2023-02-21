5680bbcdd5b2b7ce1aad0f805d9156728d371142
Neila Rodrigues <naal@gft.com>
2023-02-21 10:45:43 -0300

    Montagem tabuleiro

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,12 +1,12 @@
 package org.example.application;
 
-import org.example.bordgame.Position;
+import org.example.bordgame.Board;
+
 
 public class Program {
 
     public static void main(String[] args){
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
